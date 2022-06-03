e5fe06f78882011114134e4838098b90e64d75be
Hezekiah Oyemade <oyedey@gmail.com>
2022-06-02 18:55:30 -0700

    Using put function in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* String initialization */
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr); /* Writes the string to stdout */
+
+	return (0);
+}

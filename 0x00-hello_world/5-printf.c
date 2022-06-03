749a9dff615b26e1b191f82bf148851c51eed206
Hezekiah Oyemade <oyedey@gmail.com>
2022-06-02 19:05:22 -0700

    Using printf function in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#includes <stdio.h>
+
+int main(void)
+{
+	/* string initialization */
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+
+	return (0);
+}

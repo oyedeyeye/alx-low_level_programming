c3678bd71039d3c1e15243661a38df023743a276
Hezekiah Oyemade <oyedey@gmail.com>
2022-06-03 21:45:32 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints lowercase and uppercase alphabets
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	/* Declaration */
+	char ch, p;
+
+	/* Statement */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (p = 'A'; p <= 'Z'; p++)
+	{
+		putchar(p);
+	}
+	putchar('\n');
+
+	return (0);
+}
+

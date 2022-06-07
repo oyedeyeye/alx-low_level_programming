4e961154784a649fdc9c31ecea606fc0f660bde3
Hezekiah Oyemade <oyedey@gmail.com>
2022-06-06 21:39:46 -0700

    task 12 multiple number 2 - digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all possible combinations of two 2digit no.s
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	/* Declaration */
+	int a, b;
+
+	/* Statement */
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 98 && b == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

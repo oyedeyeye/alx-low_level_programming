008ed2981e8b10ec73446251ac6c6af9bd1e1082
Hezekiah Oyemade <oyedey@gmail.com>
2022-06-08 21:16:15 -0700

    task 2 Betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print_alphabet_x10 - prints alphabets in lower case 10 times
+ * print_alphabet_x10 - prints alphabets in lower case 10 times
  * Return: Always 0 (Success)
  */
 

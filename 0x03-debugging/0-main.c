4b0efc7451dcaacf190d034586970ab6b1ebea96
Hezekiah Oyemade <oyedey@gmail.com>
2022-06-10 20:16:22 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests the functions that prints if integer is positive or negative
+ * Description: Learning to debug by calling user defined function
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
